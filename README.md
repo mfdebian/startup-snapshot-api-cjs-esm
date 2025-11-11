54208034938aac41c8b0f3d472d6687dcd3f4cc3
Alfredo González <alfredo.arosteguy@gmail.com>
2025-11-11 16:25:04 -0300

    POC cjs to esm error in doc example

---
+++ README.md
@@ -0,0 +1,57 @@
+### CJS to ESM example
+
+When trying to run [this](https://nodejs.org/api/v8.html#startup-snapshot-api)
+example from the docs.
+
+For the CJS version it runs fine, but I can't run the ESM version.
+
+### Error
+The error I'm getting is:
+
+```bash
+import fs from 'node:fs';
+^^^^^^
+
+SyntaxError: Cannot use import statement outside a module
+    at minimalRunCjs (node:internal/main/mksnapshot:209:16)
+```
+
+Steps to reproduce:
+
+`$ cd esm/`
+
+`$ node --snapshot-blob snapshot.blob --build-snapshot entry.js`
+
+### Expected behaviour
+
+For the CJS it works as expected, steps:
+
+`$ cd cjs/`
+
+`$ node --snapshot-blob snapshot.blob --build-snapshot entry.js`
+
+The above command creates the `snapshot.blob` file.
+
+See the contents of [`book1.en_US.txt`](./cjs/book1.en_US.txt):
+
+`$ BOOK_LANG=en_US node --snapshot-blob snapshot.blob book1`
+
+Output:
+
+`> US`
+
+See the contents of [`book1.es_ES.txt`](./cjs/book1.es_ES.txt):
+
+`$ BOOK_LANG=es_ES node --snapshot-blob snapshot.blob book1`
+
+Output:
+
+`> ES`
+
+See the contents of [`book2.zh_CN.txt`](./cjs/book2.zh_CN.txt):
+
+`$ BOOK_LANG=zh_CN node --snapshot-blob snapshot.blob book2`
+
+Output:
+
+`> CN`
---
+++ esm/entry.js
@@ -0,0 +1,53 @@
+import fs from 'node:fs';
+import zlib from 'node:zlib';
+import path from 'node:path';
+import assert from 'node:assert';
+
+import v8 from 'node:v8';
+
+const __dirname = import.meta.dirname;
+
+class BookShelf {
+  storage = new Map();
+
+  // Reading a series of files from directory and store them into storage.
+  constructor(directory, books) {
+    for (const book of books) {
+      this.storage.set(book, fs.readFileSync(path.join(directory, book)));
+    }
+  }
+
+  static compressAll(shelf) {
+    for (const [ book, content ] of shelf.storage) {
+      shelf.storage.set(book, zlib.gzipSync(content));
+    }
+  }
+
+  static decompressAll(shelf) {
+    for (const [ book, content ] of shelf.storage) {
+      shelf.storage.set(book, zlib.gunzipSync(content));
+    }
+  }
+}
+
+// __dirname here is where the snapshot script is placed
+// during snapshot building time.
+const shelf = new BookShelf(__dirname, [
+  'book1.en_US.txt',
+  'book1.es_ES.txt',
+  'book2.zh_CN.txt',
+]);
+
+assert(v8.startupSnapshot.isBuildingSnapshot());
+// On snapshot serialization, compress the books to reduce size.
+v8.startupSnapshot.addSerializeCallback(BookShelf.compressAll, shelf);
+// On snapshot deserialization, decompress the books.
+v8.startupSnapshot.addDeserializeCallback(BookShelf.decompressAll, shelf);
+v8.startupSnapshot.setDeserializeMainFunction((shelf) => {
+  // process.env and process.argv are refreshed during snapshot
+  // deserialization.
+  const lang = process.env.BOOK_LANG || 'en_US';
+  const book = process.argv[1];
+  const name = `${book}.${lang}.txt`;
+  console.log((shelf.storage.get(name)).toString());
+}, shelf);
